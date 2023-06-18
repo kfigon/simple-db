f76918aacf6631b3fd1defe7dd440e5540155be6
kfigon <kamil.figon@gmail.com>
2023-06-18 17:55:22 +0200

    more storage code

--- src/storage.rs
+++ src/storage.rs
@@ -1,16 +1,30 @@
+use std::mem;
 
-#[derive(Debug)]
+
+#[derive(Debug, PartialEq)]
 struct PageHeader {
-    size: u32
+    size: usize
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Page {
     head: PageHeader,
     data: Vec<u8>
 }
 
+impl Page {
+    fn new(data: Vec<u8>) -> Self {
+        Page { 
+            head: PageHeader { size: data.len() },
+            data: data 
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct StorageError(String);
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct PageId(usize);
 
 // currently inmemory
@@ -24,12 +38,19 @@ impl StorageManager {
         StorageManager { data: Vec::new() }
     }
 
-    fn persist(&mut self, data: Vec<u8>) -> Result<PageId, StorageError> {
-        todo!()
+    fn store_new(&mut self, data: Vec<u8>) -> Result<PageId, StorageError> {
+        self.data.push(Page::new(data));
+        Ok(PageId(self.data.len()-1))
     }
 
-    fn read(&mut self, id: PageId) -> Option<Page> {
-        todo!()
+    fn update(&mut self, page_id: PageId, data: Vec<u8>) -> Result<(), StorageError> {
+        self.data.get(page_id.0).ok_or(StorageError(format!("cant find page of id {}", page_id.0)))?;
+        _ = mem::replace(&mut self.data[page_id.0], Page::new(data));
+        Ok(())
+    }
+
+    fn read(&mut self, id: PageId) -> Option<&Page> {
+        self.data.get(id.0)
     }
 }
 
@@ -37,7 +58,7 @@ fn marshall<T: serde::ser::Serialize>(data: T) -> Result<Vec<u8>, String> {
     bincode::serialize(&data).map_err(|e| format!("error during serialization {e}"))
 }
 
-fn unmarshall<'a, T: serde::de::Deserialize<'a>,>(data: &'a Vec<u8>) -> Result<T,String> {
+fn unmarshall<'a, T: serde::de::Deserialize<'a>,>(data: &'a Vec<u8>) -> Result<T, String> {
     bincode::deserialize(&data[..]).map_err(|e| format!("error during deserialization {e}"))
 }
 
@@ -81,8 +102,44 @@ mod persistance_tests {
     use super::*;
 
     #[test]
-    #[ignore]
-    fn foo() {
-        todo!()
+    fn read_unknown() {
+        let mut s = StorageManager::new();
+        assert_eq!(s.read(PageId(12)), None)
+    }
+
+    #[test]
+    fn insert_read() {
+        let mut s = StorageManager::new();
+        assert_eq!(s.read(PageId(0)), None);
+
+        s.store_new(vec![1,2,3]).unwrap();
+        assert_eq!(s.read(PageId(0)), Some(&Page::new(vec![1,2,3])));
+    }
+
+    #[test]
+    fn modify() {
+        let mut s = StorageManager::new();
+        s.store_new(vec![1,2,3]).unwrap();
+        s.store_new(vec![4,5,6]).unwrap();
+
+        assert_eq!(s.read(PageId(0)), Some(&Page::new(vec![1,2,3])));
+        assert_eq!(s.read(PageId(1)), Some(&Page::new(vec![4,5,6])));
+
+        s.update(PageId(0), vec![87]).unwrap();
+        assert_eq!(s.read(PageId(0)), Some(&Page::new(vec![87])));
+        assert_eq!(s.read(PageId(1)), Some(&Page::new(vec![4,5,6])));
+    }
+
+
+    #[test]
+    fn modify_unknown() {
+        let mut s = StorageManager::new();
+        s.store_new(vec![1,2,3]).unwrap();
+        s.store_new(vec![4,5,6]).unwrap();
+
+        assert_eq!(s.read(PageId(0)), Some(&Page::new(vec![1,2,3])));
+        assert_eq!(s.read(PageId(1)), Some(&Page::new(vec![4,5,6])));
+
+        assert!(s.update(PageId(2), vec![87]).is_err());
     }
 }
