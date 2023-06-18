aca7ef06da39bc3dcdd35cb6e8e8d24811b095b4
kfigon <kamil.figon@gmail.com>
2023-06-18 16:47:14 +0200

    basic marshalling

--- src/storage.rs
+++ src/storage.rs
@@ -1,3 +1,6 @@
+use core::slice;
+use std::{mem, io::Read};
+
 #[derive(Debug)]
 struct PageHeader {
     size: u32
@@ -32,12 +35,26 @@ impl StorageManager {
     }
 }
 
-fn marshall<T>(data: T) -> Vec<u8> {
-    todo!()
+fn marshall<T: Sized>(data: T) -> Vec<u8> {
+    let d = unsafe {
+        ::core::slice::from_raw_parts(
+            (&data as *const T) as *const u8,
+            ::core::mem::size_of::<T>(),
+        )
+    };
+    Vec::<u8>::from(d)
 }
 
-fn unmarshall<T>(data: Vec<u8>) -> T {
-    todo!()
+fn unmarshall<T>(data: Vec<u8>) -> Result<T,String> {
+    let mut out: T = unsafe { mem::zeroed() };
+
+    let struct_size = mem::size_of::<T>();
+    unsafe {
+        let slice = slice::from_raw_parts_mut(&mut out as *mut _ as *mut u8, struct_size);
+        data.as_slice().read_exact(slice).map_err(|e| format!("Error during unmarshalling: {e}"))?;
+    }
+
+    Ok(out)
 }
 
 #[cfg(test)]
@@ -46,12 +63,16 @@ mod marshalling_tests {
 
     #[test]
     fn marshall_int() {
-        assert_eq!(marshall::<i32>(1234), vec![0x12,0x34,0,0])
+        assert_eq!(marshall::<u8>(12), vec![12]);
+        assert_eq!(marshall::<i32>(1234), vec![0xd2,0x4,0,0]);
+        assert_eq!(marshall::<i32>(-1234), vec![0x2e,0xfb, 0xff,0xff]);
     }
 
     #[test]
     fn unmarshall_int() {
-        assert_eq!(unmarshall::<i32>(vec![0x12,0x34,0,0]), 1234)
+        assert_eq!(unmarshall::<u8>(vec![12]), Ok(12));
+        assert_eq!(unmarshall::<i32>(vec![0xd2,0x4,0,0]), Ok(1234));
+        assert_eq!(unmarshall::<i32>(vec![0x2e,0xfb, 0xff,0xff]), Ok(-1234));
     }
 
     #[test]
