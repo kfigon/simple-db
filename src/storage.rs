05adc29f1ac47f8e469b25e48eee3b7d6bb9a8b4
kfigon <kamil.figon@gmail.com>
2023-10-11 22:19:48 +0200

    basic persistance

--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,4 @@
-use std::mem;
+use std::{mem, collections::{HashMap, HashSet}};
 
 
 #[derive(Debug, PartialEq)]
@@ -24,18 +24,35 @@ impl Page {
 #[derive(Debug, PartialEq)]
 struct StorageError(String);
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct PageId(usize);
 
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd)]
+struct TableName(String);
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, serde::Serialize, serde::Deserialize)]
+struct FieldName(String);
+
 // currently inmemory. Storage manager == buffer pool manager here
-// todo: mutex
 struct StorageManager {
-    data: Vec<Page>
+    // todo: mutexes
+
+    // todo: move to separate Pager class
+    // ids: HashMap<usize, PageId> // todo - primary key to PageId
+    data: Vec<Page>, // all pages, full disk
+    
+    // metadata, stored in special files
+    schemas: HashMap<TableName, Vec<FieldName>>,
+    page_directory: HashMap<TableName, HashSet<PageId>>,
 }
 
 impl StorageManager {
     fn new() -> Self {
-        StorageManager { data: Vec::new() }
+        Self { 
+            data: Vec::new(), 
+            schemas: HashMap::new(), 
+            page_directory: HashMap::new(), 
+        }
     }
 
     fn store_new(&mut self, data: Vec<u8>) -> Result<PageId, StorageError> {
@@ -51,6 +68,22 @@ impl StorageManager {
 
     fn read(&mut self, id: PageId) -> Option<&Page> {
         self.data.get(id.0)
+    }
+
+    fn insert_data(&mut self, table_name: TableName, data: HashMap<FieldName, String>) -> Result<PageId, StorageError> {
+        // todo: validate schema
+        let page_id = self.store_new(marshall(data)?)?;
+        let page_ids = self.page_directory.entry(table_name).or_insert(HashSet::default());
+        page_ids.insert(page_id);
+        
+        Ok(page_id)
+    }
+
+    fn update_data(&mut self, id: PageId, data: HashMap<FieldName, String>) -> Result<PageId, StorageError> {
+        // todo: validate schema
+
+        self.update(id, marshall(data)?)?;
+        Ok(id)
     }
 }
 
@@ -141,5 +174,48 @@ mod persistance_tests {
         assert_eq!(s.read(PageId(1)), Some(&Page::new(vec![4,5,6])));
 
         assert!(s.update(PageId(2), vec![87]).is_err());
+    }
+
+    #[test]
+    fn insert() {
+        let data = HashMap::from([
+            (FieldName("foo".to_string()), "1234".to_string()),
+            (FieldName("bar".to_string()), "the value".to_string()),
+        ]);
+        let table_name = TableName("the_table".to_string());
+
+        let mut s = StorageManager::new();
+        let id = s.insert_data(table_name, data).unwrap();
+        
+        let p = s.read(id).unwrap();
+        let read_data: HashMap<FieldName, String> = unmarshall(&p.data).unwrap();
+        assert_eq!(read_data, HashMap::from([
+            (FieldName("foo".to_string()), "1234".to_string()),
+            (FieldName("bar".to_string()), "the value".to_string()),
+        ]));
+    }
+
+    #[test]
+    fn update() {
+        let data = HashMap::from([
+            (FieldName("foo".to_string()), "1234".to_string()),
+            (FieldName("bar".to_string()), "the value".to_string()),
+        ]);
+        let table_name = TableName("the_table".to_string());
+
+        let mut s = StorageManager::new();
+        let id = s.insert_data(table_name, data).unwrap();
+        
+        s.update_data(id, HashMap::from([
+            (FieldName("fooooo".to_string()), "1234".to_string()),
+            (FieldName("barooo".to_string()), "the value".to_string()),
+        ]));
+
+        let p = s.read(id).unwrap();
+        let read_data: HashMap<FieldName, String> = unmarshall(&p.data).unwrap();
+        assert_eq!(read_data, HashMap::from([
+            (FieldName("fooooo".to_string()), "1234".to_string()),
+            (FieldName("barooo".to_string()), "the value".to_string()),
+        ]));
     }
 }
