c2583d8170b044d09f5519c273b08e42eac5a70e
kfigon <kamil.figon@gmail.com>
2023-12-22 22:02:06 +0100

    search

--- go-attempt/README.md
+++ go-attempt/README.md
@@ -7,9 +7,17 @@ based on [chidb](http://chi.cs.uchicago.edu/chidb/index.html) from university of
 * btrees for indexes
 
 ## btree
+
+b - balanced
+
 * [Let's Build a Simple Database](https://cstack.github.io/db_tutorial/)
 * [YT playlist](https://www.youtube.com/playlist?list=PLA5Lqm4uh9Bbq-E0ZnqTIa8LRaL77ica6)
 * [wiki](https://en.wikipedia.org/wiki/B-tree)
 * [baeldung](https://www.baeldung.com/cs/b-tree-data-structure)
-* [nice video](https://www.youtube.com/watch?v=SI6E4Ma2ddg)
+* [nice btree explanation video](https://www.youtube.com/watch?v=SI6E4Ma2ddg)
 * [btree vs b+tree discussion in databases](https://www.youtube.com/watch?v=UzHl2VzyZS4)
+* [indexing talk](https://www.youtube.com/watch?v=HubezKbFL7E)
+    * order of columns in index matters (The moment it encounters an inequality, the indexing stops right there)
+    * functions == full table scan
+    * inequality operators matters
+    * add relevant columns even from select to index
--- go-attempt/btree/tree.go
+++ go-attempt/btree/tree.go
@@ -23,18 +23,18 @@ type BTree struct{
 func NewBtree(order int) *BTree {
 	return &BTree {
 		order: order,
+		root: &node{
+			keys:      nil,
+			isLeaf:    true,
+			children:  nil,
+		},
 	}
 }
 
-func (b *BTree)insert(key int) {
-	if b.root == nil {
-		b.root = &node{
-			keys:      nil,
-			isLeaf:    true,
-			children:  []*node{},
-		}
-	}
-	
+// https://iq.opengenus.org/b-tree-in-python/
+// https://gist.github.com/mateor/885eb950df7231f178a5
+// https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/BTree.java.html
+func (b *BTree)insert(key int) {	
 	if len(b.root.keys) < (b.order-1) {
 		b.root.keys = append(b.root.keys, key)
 		slices.Sort(b.root.keys)
@@ -48,7 +48,20 @@ func (b *BTree)insert(key int) {
 }
 
 func (b *BTree)search(key int) (int, bool) {
-	return 0, false
+	var fn func(*node) (int, bool)
+	fn = func(n *node) (int, bool) {
+		i := 0
+		for i < len(n.keys) && key > n.keys[i] {
+			i += 1
+		}
+		if i < len(n.keys) && key == n.keys[i] {
+			return n.keys[i], true
+		} else if n.isLeaf {
+			return 0, false
+		}
+		return fn(b.root.children[i])
+	}
+	return fn(b.root)
 }
 
 func (b *BTree)delete(key int) bool {
