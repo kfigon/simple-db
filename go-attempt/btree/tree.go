d76ae214f29cb17eec6c13e019d1e8ec17c91351
kfigon <kamil.figon@gmail.com>
2023-12-26 21:19:16 +0100

    pending impl

--- go-attempt/btree/tree.go
+++ go-attempt/btree/tree.go
@@ -14,7 +14,6 @@ type node struct{
 	children []*node
 }
 
-
 type BTree struct{
 	order int // number of children
 	root *node
@@ -31,36 +30,63 @@ func NewBtree(order int) *BTree {
 	}
 }
 
+func (b *BTree) full(n *node) bool {
+	return len(n.keys) >= b.order-1
+}
+
 // https://iq.opengenus.org/b-tree-in-python/
 // https://gist.github.com/mateor/885eb950df7231f178a5
 // https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/BTree.java.html
-func (b *BTree)insert(key int) {	
-	if len(b.root.keys) < (b.order-1) {
-		b.root.keys = append(b.root.keys, key)
-		slices.Sort(b.root.keys)
+
+// https://www.youtube.com/watch?v=tT2DT9Z4H-0&list=PL9xmBV_5YoZNFPPv98DjTdD9X6UI9KMHz&index=5
+func (b *BTree)insert(key int) {
+	if b.full(b.root) {
+		oldRoot := b.root
+		b.root = &node{}
+		b.root.children = append(b.root.children, oldRoot)
+		b.split(b.root, 0)
+		b.insertNonFull(b.root, key)
+		return
+	}
+	b.insertNonFull(b.root, key)
+	
+}
+
+func (b *BTree) insertNonFull(n *node, key int) {
+	if n.isLeaf {
+		n.keys = append(n.keys, key)
+		slices.Sort(n.keys)
 		return
 	}
 
-	nextNode := b.root
-	for nextNode != nil {
-		break
+	i := 0
+	for i < len(n.keys) && key > n.keys[i] {
+		i++
 	}
+	if b.full(n) {
+		b.split(n, i)
+		if key > n.keys[i] {
+			i++
+		}
+	}
+	b.insertNonFull(n.children[i], key)
 }
 
 func (b *BTree) split(parent *node, fullChildIdx int) {
 	// todo: not finished, check ranges
-	
 	fullChild := parent.children[fullChildIdx]
+	medianId := len(fullChild.keys)/2
+
 
 	newNode := &node{ isLeaf: fullChild.isLeaf }
 	// todo: sort both
 	parent.children = append(parent.children, newNode)
 
 	// insert median
-	parent.keys = append(parent.keys, fullChild.keys[len(fullChild.keys)/2])
+	parent.keys = append(parent.keys, fullChild.keys[medianId])
 
-	newNode.keys = fullChild.keys[1+len(fullChild.keys)/2:]
-	fullChild.keys = fullChild.keys[:len(fullChild.keys)/2]
+	newNode.keys = fullChild.keys[1+medianId:]
+	fullChild.keys = fullChild.keys[:medianId]
 
 	if !fullChild.isLeaf {
 		newNode.children = fullChild.children[1+len(fullChild.children)/2:]
@@ -73,7 +99,7 @@ func (b *BTree)search(key int) (int, bool) {
 	fn = func(n *node) (int, bool) {
 		i := 0
 		for i < len(n.keys) && key > n.keys[i] {
-			i += 1
+			i++
 		}
 		if i < len(n.keys) && key == n.keys[i] {
 			return n.keys[i], true
