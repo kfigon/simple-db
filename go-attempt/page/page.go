accc885988a7cf078cd857ad0dbf5773b2c76958
kfigon <kamil.figon@gmail.com>
2024-09-17 21:42:15 +0200

    directory improvements

--- go-attempt/page/page.go
+++ go-attempt/page/page.go
@@ -13,7 +13,7 @@ const PageSize = 8*512
 
 type PageId int // 1 base-indexed
 type PageOffset int
-type PageType byte
+type PageType byte // always first byte in a page
 const (
 	RootPageType PageType = iota + 1
 	DirectoryPageType
@@ -32,6 +32,16 @@ type RootPage struct {
 	SchemaPageRootID PageId
 }
 
+func NewRootPage() *RootPage{
+	n := 0xDEADBEEF
+	return &RootPage{
+		PageTyp: RootPageType,
+		MagicNumber: I32(n),
+		DirectoryPageRootID: 0,
+		SchemaPageRootID: 0,
+	}
+}
+
 // =================== directory. 
 // contains info about the content of the db
 // where to find all pages and lookup what table's inside
@@ -44,9 +54,21 @@ type DirectoryPage struct {
 	PagesData []PageCatalog
 }
 
+func NewDirectoryPage() *DirectoryPage {
+	return &DirectoryPage{
+		Header:    struct{PageTyp PageType; NextPage PageId; PageDataLen I16}{
+			PageTyp: DirectoryPageType,
+			NextPage: 0,
+			PageDataLen: 0,
+		},
+		PagesData: nil,
+	}
+}
+
 type PageCatalog struct {
 	StartPageID PageId
-	ObjectName String 
+	ObjectType PageType // what kind of page is it
+	ObjectName String
 }
 
 // ============ Schema 
