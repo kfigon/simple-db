1f25c7071ddde8ff687335974bfe2b6182372428
kfigon <kamil.figon@gmail.com>
2024-09-28 10:45:29 +0200

    in memory pager

--- go-attempt/storagemanager/inmemory.go
+++ go-attempt/storagemanager/inmemory.go
@@ -8,11 +8,18 @@ type InMemoryPager struct {
 	data []byte
 }
 
+func NewInMemoryPager() *InMemoryPager {
+	return &InMemoryPager{
+		data: make([]byte, 30*page.PageSize),
+	}
+}
+
 func (i *InMemoryPager) ReadPage(pageId page.PageId) []byte {
 	startIdx := pageId*page.PageSize
 	return i.data[startIdx:startIdx+page.PageSize]
 }
 
+// todo: add grow method
 func (i *InMemoryPager) WritePage(pageId page.PageId, data []byte) error {
 	startIdx := pageId*page.PageSize
 	copy(i.data[startIdx:], data)
