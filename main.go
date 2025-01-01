d9d896776cf58ca40f67e3631d8ff709f92640f1
kfigon <kamil.figon@gmail.com>
2025-01-01 16:18:42 +0100

    dummy cli

--- main.go
+++ main.go
@@ -1,10 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"simple-db/naive"
+	"simple-db/sql"
+	"strings"
 )
 
-
 func main() {
-	fmt.Println("hello")
+	fmt.Println("hello, type commands, 'quit' to stop")
+	storage := naive.NewStorage()
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		
+		s, err := reader.ReadString('\n')
+		s = strings.TrimSpace(s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		} else if s == "quit" || s == "exit" {
+			fmt.Println("auf wiedersehen!")
+			break
+		}
+
+		stmt, err := sql.Parse(sql.Lex(s))
+		if err != nil {
+			fmt.Println("error:", err)
+			continue
+		}
+
+		switch stmt := stmt.(type) {
+		case *sql.CreateStatement:
+			if err = storage.CreateTable(*stmt); err != nil {
+				fmt.Println(err)
+			}
+		case *sql.InsertStatement:
+			if err = storage.Insert(*stmt); err != nil {
+				fmt.Println(err)
+			}
+		case *sql.SelectStatement:
+			res, err := storage.Select(*stmt)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println(res)
+			}
+		}
+	}
 }
