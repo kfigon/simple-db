20103c0097cefedbd5fa191849f6bfa17dd974e3
kfigon <kamil.figon@gmail.com>
2025-02-23 13:06:14 +0100

    slotted page mostly done

---
+++ naive/slottedpage.go
@@ -0,0 +1,96 @@
+package naive
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// id within the slotpage
+type RowId int
+
+type Slotted struct {
+	Indexes []int // RowId -> offset within page
+	CellData []byte
+	lastOffset int
+}
+
+func NewSlotted(slottedPageSize int) *Slotted {
+	return &Slotted{
+		lastOffset: slottedPageSize,
+		CellData: make([]byte, slottedPageSize), // redundant, as not counting slot array size
+	}
+}
+
+var errNoSpace = fmt.Errorf("no space in slot array")
+
+func (s *Slotted) Add(buf []byte) (RowId, error) {
+	bytesWithHeader := SerializeBytes(buf)
+	ln := len(bytesWithHeader)
+
+	if !s.hasSpace(ln) {
+		return 0, errNoSpace
+	}
+	copy(s.CellData[s.lastOffset - ln:], bytesWithHeader)
+	s.lastOffset -= ln
+
+	s.Indexes = append(s.Indexes, s.lastOffset)
+
+	return RowId(len(s.Indexes) - 1), nil
+}
+
+func (s *Slotted) Put(id RowId, buf []byte) (RowId, error) {
+	// todo
+	return 0, nil
+}
+
+func (s *Slotted) Read(idx RowId) ([]byte, error) {
+	if int(idx) >= len(s.Indexes) {
+		return nil, fmt.Errorf("invalid idx %d, got only %d", idx, len(s.Indexes))
+	}
+
+	offset := s.Indexes[idx]
+	return DeserializeBytes(BytesWithHeader(s.CellData[offset:]))
+}
+
+func (s *Slotted) hasSpace(newData int) bool {
+	const rowIdSize = 2
+	return s.lastOffset - newData - (len(s.Indexes) * rowIdSize)  > 0
+}
+
+// todo: when serializing wrapping page - remember to add size of slot array
+func (s *Slotted) Serialize() []byte {
+	var buf bytes.Buffer
+	for _, id := range s.Indexes {
+		buf.Write(SerializeInt(int32(id)))
+	}
+
+	paddingLen := s.lastOffset - len(s.Indexes)*4
+
+	buf.Write(make([]byte, paddingLen))
+	buf.Write(s.CellData[s.lastOffset:])
+
+	return buf.Bytes()
+}
+
+func DeserializeSlotted(b []byte, slotArrayLen int) (*Slotted, error) {
+	originalSlice := b
+
+	p := NewSlotted(len(b))
+	var lastOffset *int
+	
+	for range slotArrayLen {
+		i, err := DeserializeIntAndEat(&b)
+		if err != nil {
+			return nil, fmt.Errorf("error deserializing slot array: %w", err)
+		}
+		p.Indexes = append(p.Indexes, i)
+
+		if lastOffset == nil || *lastOffset > i { // just min
+			lastOffset = &i
+		}
+	}
+	p.lastOffset = *lastOffset
+	copy(p.CellData[*lastOffset:], originalSlice[*lastOffset:])
+
+	return p, nil
+}
--- naive/slottedpage_test.go
+++ naive/slottedpage_test.go
@@ -1,7 +1,6 @@
 package naive
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,17 +25,6 @@ func TestSlotted(t *testing.T) {
 	})
 }
 
-func initWithData(t *testing.T, p *Slotted) map[RowId]string {
-	inserted := map[RowId]string{}
-
-	for _, inStr := range []string{"hello", "world", "foobar"} {
-		id, err := p.Add([]byte(inStr))
-		assert.NoError(t, err)
-		inserted[id] = inStr
-	}
-	return inserted
-}
-
 func TestSerialization(t *testing.T) {
 	t.Run("serialization", func(t *testing.T) {
 		p := NewSlotted(50)
@@ -56,7 +44,7 @@ func TestSerialization(t *testing.T) {
 		inserted := initWithData(t, p)
 		data := p.Serialize()
 		
-		newP, err := DeserializeSlotted(data)
+		newP, err := DeserializeSlotted(data, len(inserted))
 		assert.NoError(t, err)
 
 		assert.Len(t, newP.Indexes, 3)
@@ -66,66 +54,17 @@ func TestSerialization(t *testing.T) {
 	
 			assert.Equal(t, []byte(expStr), got)
 		}
+		assert.Equal(t, p, newP)
 	})
 }
 
-// id within the slotpage
-type RowId int
+func initWithData(t *testing.T, p *Slotted) map[RowId]string {
+	inserted := map[RowId]string{}
 
-type Slotted struct {
-	Indexes []int // RowId -> offset within page
-	CellData []byte
-	lastOffset int
-}
-
-func NewSlotted(slottedPageSize int) *Slotted {
-	return &Slotted{
-		lastOffset: slottedPageSize,
-		CellData: make([]byte, slottedPageSize),
+	for _, inStr := range []string{"hello", "world", "foobar"} {
+		id, err := p.Add([]byte(inStr))
+		assert.NoError(t, err)
+		inserted[id] = inStr
 	}
-}
-
-var errNoSpace = fmt.Errorf("no space in slot array")
-
-func (s *Slotted) Add(buf []byte) (RowId, error) {
-	bytesWithHeader := SerializeBytes(buf)
-	ln := len(bytesWithHeader)
-
-	if !s.hasSpace(ln) {
-		return 0, errNoSpace
-	}
-	copy(s.CellData[s.lastOffset - ln:], bytesWithHeader)
-	s.lastOffset -= ln
-
-	s.Indexes = append(s.Indexes, s.lastOffset)
-
-	return RowId(len(s.Indexes) - 1), nil
-}
-
-func (s *Slotted) Put(id RowId, buf []byte) (RowId, error) {
-	// todo
-	return 0, nil
-}
-
-func (s *Slotted) Read(idx RowId) ([]byte, error) {
-	if int(idx) >= len(s.Indexes) {
-		return nil, fmt.Errorf("invalid idx %d, got only %d", idx, len(s.Indexes))
-	}
-
-	offset := s.Indexes[idx]
-	return DeserializeBytes(BytesWithHeader(s.CellData[offset:]))
-}
-
-func (s *Slotted) hasSpace(newData int) bool {
-	const rowIdSize = 2
-	return s.lastOffset - newData - (len(s.Indexes) * rowIdSize)  > 0
-}
-
-func (s *Slotted) Serialize() []byte {
-	// indexes, padding, cells
-	return nil
-}
-
-func DeserializeSlotted(b []byte) (*Slotted, error) {
-	return nil, nil
+	return inserted
 }
