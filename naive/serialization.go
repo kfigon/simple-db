9c0ae3bc269e55e851dfbc598c9523ea46c20146
kfigon <kamil.figon@gmail.com>
2025-03-22 17:23:45 +0100

    code cleanup and rename

--- naive/serialization.go
+++ naive/serialization.go
@@ -2,6 +2,7 @@ package naive
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -80,4 +81,106 @@ func DeserializeAll[T any](b []byte, funs ...demapper[T]) (*T, error) {
 		}	
 	}
 	return &out, nil
+}
+
+var endinanness = binary.BigEndian
+
+type BytesWithHeader []byte
+
+func SerializeBool(b bool) []byte {
+	if b {
+		return []byte{1}
+	}
+	return []byte{0}
+}
+
+func SerializeInt(i int32) []byte {
+	return endinanness.AppendUint32([]byte{}, uint32(i))
+}
+
+func SerializeString(s string) []byte {
+	return SerializeBytes([]byte(s))
+}
+
+func SerializeBytes(b []byte) BytesWithHeader {
+	header := SerializeInt(int32(len(b)))
+	return append(header, b...)
+}
+
+func DeserializeBool(b []byte) (bool, error) {
+	switch {
+	case len(b) < 1:
+		return false, deserializationErr(len(b), 1, "bool")
+	case b[0] == 0:
+		return false, nil
+	case b[0] == 1:
+		return true, nil
+	default:
+		return false, fmt.Errorf("invalid boolean, expected 0 or 1, got %v", b[0])
+	}
+}
+
+func DeserializeInt(b []byte) (int32, error) {
+	exp := 4
+	got := len(b)
+	if got < exp {
+		return 0, deserializationErr(got, exp, "i32")
+	}
+	return int32(endinanness.Uint32(b)), nil
+}
+
+func DeserializeString(b []byte) (string, error) {
+	v, err := DeserializeBytes(b)
+	if err != nil {
+		return "", err
+	}
+	return string(v), nil
+}
+
+func DeserializeBytes(b BytesWithHeader) ([]byte, error) {
+	exp := 4
+	got := len(b)
+	if got < exp {
+		return nil, deserializationErr(got, exp, "array header")
+	}
+	howMany, err := DeserializeInt(b[:4])
+	if err != nil {
+		return nil, fmt.Errorf("error deserializing header length: %w", err)
+	}
+	return b[4 : 4+howMany], nil
+}
+
+func deserializationErr(got, exp int, typ string) error {
+	return fmt.Errorf("cant deserialize into %v, expected lenght %v, got %v", typ, exp, got)
+}
+
+func DeserializeStringAndEat(b *[]byte) (string, error) {
+	v, err := DeserializeString(*b)
+	if err != nil {
+		return "", err
+	}
+	advanceBuf(b, 4+len(v))
+	return v, nil
+}
+
+func DeserializeBoolAndEat(b *[]byte) (bool, error) {
+	v, err := DeserializeBool(*b)
+	if err != nil {
+		return false, err
+	}
+	advanceBuf(b, 1)
+	return v, nil
+}
+
+func DeserializeIntAndEat(b *[]byte) (int32, error) {
+	v, err := DeserializeInt(*b)
+	if err != nil {
+		return 0, err
+	}
+	advanceBuf(b, 4)
+	return v, nil
+}
+
+func advanceBuf(b *[]byte, howMany int) {
+	*b = (*b)[howMany:]
 }
