3dcb696209f2e82ca1cdedfd0b346d31341e1793
kfigon <kamil.figon@gmail.com>
2025-04-20 22:56:28 +0200

    small refactoring

--- naive/storage.go
+++ naive/storage.go
@@ -306,36 +306,19 @@ func castAndBinaryOp[T any](left,right any, op func(T,T)bool) bool {
 	return op(lV, rV)
 }
 
-func eq[T comparable](a,b T) bool {
-	return a == b
-}
+func eq[T comparable](a,b T) bool { return a == b }
+func neq[T comparable](a,b T) bool { return a != b }
+func or(a,b bool) bool { return a || b }
+func and(a,b bool) bool { return a && b }
+func gt[T cmp.Ordered](a,b T) bool { return a > b }
+func geq[T cmp.Ordered](a,b T) bool { return a >= b }
+func lt[T cmp.Ordered](a,b T) bool { return a < b }
+func leq[T cmp.Ordered](a,b T) bool { return a <= b }
 
-func neq[T comparable](a,b T) bool {
-	return a != b
-}
-
-func or(a,b bool) bool {
-	return a || b
-}
-
-func and(a,b bool) bool {
-	return a && b
-}
-
-func gt[T cmp.Ordered](a,b T) bool {
-	return a > b
-}
-
-func geq[T cmp.Ordered](a,b T) bool {
-	return a >= b
-}
-
-func lt[T cmp.Ordered](a,b T) bool {
-	return a < b
-}
-
-func leq[T cmp.Ordered](a,b T) bool {
-	return a <= b
+func buildCastAndOperand[T any](left, right ColumnData, fn func(a,b T)bool) func()bool {
+	return func() bool {
+		return castAndBinaryOp(left.Data, right.Data, fn)
+	}
 }
 
 func predBuilder(pred sql.BoolExpression, r Row) ColumnData {
@@ -352,19 +335,19 @@ func predBuilder(pred sql.BoolExpression, r Row) ColumnData {
 
 		op := map[string]map[FieldType]func()bool {
 			"=": {
-				String: func() bool { return castAndBinaryOp(left.Data, right.Data, eq[string]) },
-				Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, eq[int32]) },
-				Boolean: func() bool { return castAndBinaryOp(left.Data, right.Data, eq[bool]) },
+				String: buildCastAndOperand(left, right, eq[string]),
+				Int32: buildCastAndOperand(left, right, eq[int32]),
+				Boolean: buildCastAndOperand(left, right, eq[bool]),
 			},
 			"!=": {
-				String: func() bool { return castAndBinaryOp(left.Data, right.Data, neq[string]) },
-				Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, neq[int32]) },
-				Boolean: func() bool { return castAndBinaryOp(left.Data, right.Data, neq[bool]) },
+				String: buildCastAndOperand(left, right, neq[string]),
+				Int32: buildCastAndOperand(left, right, neq[int32]),
+				Boolean: buildCastAndOperand(left, right, neq[bool]),
 			},
-			">": { Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, gt[int32]) }, },
-			">=": { Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, geq[int32]) }, },
-			"<": { Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, lt[int32]) }, },
-			"<=": { Int32: func() bool { return castAndBinaryOp(left.Data, right.Data, leq[int32]) }, },
+			">": { Int32: buildCastAndOperand(left,right, gt[int32])},
+			">=": { Int32: buildCastAndOperand(left,right, geq[int32])},
+			"<": { Int32: buildCastAndOperand(left,right, lt[int32])},
+			"<=": { Int32: buildCastAndOperand(left,right, leq[int32])},
 		}
 
 		ops, ok := op[v.Operator.Lexeme]
