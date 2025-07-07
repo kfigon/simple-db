e8968fd078744c2604214e8003621b3470796832
kfigon <kamil.figon@gmail.com>
2025-07-07 17:55:00 +0200

    Revert "minor refactoring, some files moved around"
    This reverts commit 39808e733e54a4adc0143a3d1fdb5a3d271ff21b.

--- naive/storage.go
+++ naive/storage.go
@@ -2,17 +2,59 @@ package naive
 
 import (
 	"bytes"
+	"cmp"
 	"errors"
 	"fmt"
 	"io"
 	"simple-db/sql"
+	"strconv"
 )
 
 const PageSize = 4 * 1024
 const directoryName = "catalog_directory"
 const assertionsEnabled = true
 
+type FieldType int32
+
+const (
+	Int32 FieldType = iota
+	String
+	Boolean
+	Float
+)
+
+func (f FieldType) String() string {
+	return [...]string{
+		"Int32",
+		"String",
+		"Boolean",
+		"Float",
+	}[f]
+}
+
+func FieldTypeFromString(s string) (FieldType, error) {
+	switch s {
+	case "int":
+		return Int32, nil
+	case "string":
+		return String, nil
+	case "boolean":
+		return Boolean, nil
+	case "float":
+		return Float, nil
+	default:
+		return 0, fmt.Errorf("invalid type %v", s)
+	}
+}
+
 type TableName string
+type FieldName string
+
+type ColumnData struct {
+	Typ  FieldType
+	Data any
+}
+
 type TableSchema map[FieldName]FieldType
 type Schema map[TableName]TableSchema
 type Row map[FieldName]ColumnData
@@ -197,6 +239,21 @@ func (s *Storage) Insert(stmt sql.InsertStatement) error {
 	return nil
 }
 
+func parseType(v string, typ FieldType) (any, error) {
+	switch typ {
+	case Int32:
+		v, err := strconv.ParseInt(v, 10, 32)
+		return int32(v), err
+	case String:
+		return v, nil
+	case Boolean:
+		return strconv.ParseBool(v)
+	case Float:
+		return strconv.ParseFloat(v, 64)
+	default:
+		return nil, fmt.Errorf("invalid data type %v", typ)
+	}
+}
 
 type QueryResult struct {
 	Header []FieldName
@@ -243,6 +300,88 @@ func (s *Storage) Select(stmt sql.SelectStatement) (QueryResult, error) {
 	return out, nil
 }
 
+// todo: add error handling
+func buildPredicate(pred sql.BoolExpression) func(Row) bool {
+	return func(r Row) bool {
+		col := predBuilder(pred, r)
+		debugAssert(col.Typ == Boolean, "boolean predicate required, got %v", col.Typ)
+		return col.Data.(bool)
+	}
+}
+
+func castAndBinaryOp[T any](left, right any, op func(T, T) bool) bool {
+	lV := left.(T)
+	rV := right.(T)
+	return op(lV, rV)
+}
+
+func eq[T comparable](a, b T) bool   { return a == b }
+func neq[T comparable](a, b T) bool  { return a != b }
+func or(a, b bool) bool              { return a || b }
+func and(a, b bool) bool             { return a && b }
+func gt[T cmp.Ordered](a, b T) bool  { return a > b }
+func geq[T cmp.Ordered](a, b T) bool { return a >= b }
+func lt[T cmp.Ordered](a, b T) bool  { return a < b }
+func leq[T cmp.Ordered](a, b T) bool { return a <= b }
+
+func buildCastAndOperand[T any](left, right ColumnData, fn func(a, b T) bool) func() bool {
+	return func() bool {
+		return castAndBinaryOp(left.Data, right.Data, fn)
+	}
+}
+
+func predBuilder(pred sql.BoolExpression, r Row) ColumnData {
+	switch v := pred.(type) {
+	case *sql.InfixExpression:
+		left := predBuilder(v.Left, r)
+		right := predBuilder(v.Right, r)
+
+		if v.Operator.Lexeme == "and" {
+			return ColumnData{Boolean, castAndBinaryOp(left.Data, right.Data, and)}
+		} else if v.Operator.Lexeme == "or" {
+			return ColumnData{Boolean, castAndBinaryOp(left.Data, right.Data, or)}
+		}
+
+		op := map[string]map[FieldType]func() bool{
+			"=": {
+				String:  buildCastAndOperand(left, right, eq[string]),
+				Int32:   buildCastAndOperand(left, right, eq[int32]),
+				Boolean: buildCastAndOperand(left, right, eq[bool]),
+			},
+			"!=": {
+				String:  buildCastAndOperand(left, right, neq[string]),
+				Int32:   buildCastAndOperand(left, right, neq[int32]),
+				Boolean: buildCastAndOperand(left, right, neq[bool]),
+			},
+			">":  {Int32: buildCastAndOperand(left, right, gt[int32])},
+			">=": {Int32: buildCastAndOperand(left, right, geq[int32])},
+			"<":  {Int32: buildCastAndOperand(left, right, lt[int32])},
+			"<=": {Int32: buildCastAndOperand(left, right, leq[int32])},
+		}
+
+		ops, ok := op[v.Operator.Lexeme]
+		debugAssert(ok, "unsupported op: %v", v.Operator)
+		debugAssert(left.Typ == right.Typ, "incompatible types %v %v", left.Typ, right.Typ)
+
+		fn, ok := ops[left.Typ]
+		debugAssert(ok, "unknown type %v", left.Typ)
+		return ColumnData{Boolean, fn()}
+	case sql.ValueLiteral:
+		if v.Tok.Typ == sql.Number {
+			return ColumnData{Int32, int32(must(strconv.Atoi(v.Tok.Lexeme)))}
+		} else if v.Tok.Typ == sql.String {
+			return ColumnData{String, v.Tok.Lexeme}
+		} else if v.Tok.Typ == sql.Boolean {
+			return ColumnData{Boolean, must(strconv.ParseBool(v.Tok.Lexeme))}
+		}
+		debugAssert(false, "unsupported type %v", v)
+	case sql.ColumnLiteral:
+		return r[FieldName(v.Name.Lexeme)]
+	}
+
+	debugAssert(false, "unknown predicate type received %T", pred)
+	panic("")
+}
 
 func (s *Storage) AllSchema() Schema {
 	schema := Schema{}
@@ -337,4 +476,19 @@ func DeserializeDb(r io.Reader) (*Storage, error) {
 		pages = append(pages, p)
 	}
 	return &Storage{root: *root, allPages: pages}, nil
+}
+
+func must[T any](v T, err error) T {
+	debugAsserErr(err, "expected no error")
+	return v
+}
+
+func debugAssert(expectTrue bool, format string, args ...any) {
+	if assertionsEnabled && !expectTrue {
+		panic(fmt.Sprintf(format, args...))
+	}
+}
+
+func debugAsserErr(err error, format string, args ...any) {
+	debugAssert(err == nil, format, args...)
 }
